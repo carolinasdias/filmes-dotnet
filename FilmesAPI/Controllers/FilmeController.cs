fcb56c71f47b9acb003294150f68c94a38209c84
carolinasdias <carolinasdias06@gmail.com>
2024-05-17 16:08:02 -0300

    adicionando método buscar por id

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -10,14 +10,32 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     { 
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
-    [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        [HttpPost]
+        public IActionResult AdicionaFilme(
+        [FromBody] Filme filme)
         {
-        filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorId),
+                new { id = filme.Id },
+                filme);
         }
 
-}
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,
+        [FromQuery] int take = 50)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            return Ok(filme);
+        }
+    }
 }
--- FilmesAPI/obj/Debug/net8.0/FilmesAPI.AssemblyInfo.cs
+++ FilmesAPI/obj/Debug/net8.0/FilmesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e033822ba719cded0b4061238a573086567873")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
