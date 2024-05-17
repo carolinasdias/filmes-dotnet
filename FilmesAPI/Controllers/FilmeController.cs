82e033822ba719cded0b4061238a573086567873
carolinasdias <carolinasdias06@gmail.com>
2024-05-17 15:50:39 -0300

    criando Filme controler e model

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    { 
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+        filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+
+}
+}
